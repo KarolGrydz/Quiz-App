578de896518f27bf328c242d586ab6a97ac471cb
Karol Grydz <grydzkarol@gmail.com>
2020-08-24 13:24:29 +0200

    UPDATE

--- src/App.tsx
+++ src/App.tsx
@@ -51,17 +51,24 @@ const App = () => {
       ) : null}
       {!gameOver && <p className="score">Score: </p>}
       {loading && <p>Loading questions...</p>}
-      {/* <QuestionCard
-        questionNr={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
-      <button className="next" onClick={nextQuestion}>
-        Next question
-      </button>
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next question
+        </button>
+      ) : null}
     </div>
   );
 };
